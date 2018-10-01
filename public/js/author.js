1c39a4f64dc2fe9d0088133cf362847ac1268ccf
randy <randyaakhus@gmail.com>
2018-10-01 14:38:02 -0700

    figuring out our system

--- public/js/author.js
+++ public/js/author.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
   function handleAuthorFormSubmit(event) {
     event.preventDefault();
     // Don't do anything if the name fields hasn't been filled out
-    if (!nameInput.val().trim().trim()) {
+    if (!nameInput.val().trim()) {
       return;
     }
     // Calling the upsertAuthor function and passing in the value of the name input
